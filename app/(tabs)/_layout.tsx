29998ff7a9ea5e7b9c25552fa9938b798a2a6c6c
Pemba Sherpa <norsangpemba@gmail.com>
2024-09-26 11:18:01 -0400

    update setting icon

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -33,7 +33,7 @@ export default function TabLayout() {
           title: "Setting",
           tabBarIcon: ({ color, focused }) => (
             <TabBarIcon
-              name={focused ? "code-slash" : "code-slash-outline"}
+              name={focused ? "settings" : "settings-outline"}
               color={color}
             />
           ),
