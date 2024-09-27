3ef2fa61c79315fffc6827f45e65649868275ffe
Pemba Sherpa <norsangpemba@gmail.com>
2024-09-27 10:54:39 -0400

    remove old eventlistener and register new eventlistener when authManager is updated

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,14 @@
 // App.js
 
 import React, { useEffect } from "react";
-import { View, Button, Linking, Alert, StyleSheet } from "react-native";
+import {
+  View,
+  Button,
+  Linking,
+  Alert,
+  StyleSheet,
+  EmitterSubscription,
+} from "react-native";
 import OAuthManager from "../../services/OAuthManager";
 import { openBrowserAsync } from "expo-web-browser";
 import { useAuthManager } from "@/store/useAuthManager";
@@ -18,40 +25,48 @@ const App = () => {
       SecureKeyStore.getKey(CustomKeyType.ACCESS_TOKEN_SECRET)
     )
       setHasAccessToken(true);
-    if (manager) return;
-
-    let oauthManager: OAuthManager;
-    try {
-      oauthManager = new OAuthManager();
-      setManager(oauthManager);
-    } catch (error) {
-      console.error("Error creating OAuthManager", error);
+    if (!manager) {
+      let oauthManager: OAuthManager;
+      try {
+        oauthManager = new OAuthManager();
+        setManager(oauthManager);
+      } catch (error) {
+        console.error("Error creating OAuthManager", error);
+      }
     }
 
-    // Handle OAuth callback
-    const handleUrl = (event: { url: string }) => {
-      const url = event.url;
-      if (url.startsWith("exp://192.168.2.83:8081")) {
-        const params = new URLSearchParams(url.split("?")[1]);
-        const oauth_verifier = params.get("oauth_verifier");
-
-        // Step 5: Exchange the request token for an access token
-        oauthManager
-          .getAccessToken(oauth_verifier)
-          .then((success) => {
-            if (success) {
-              Alert.alert("Access Granted");
-              setHasAccessToken(true);
-            }
-          })
-          .catch((error) => {
-            console.error("Error getting access token", error);
-          });
-      }
+    let eventListen: EmitterSubscription;
+    if (manager) {
+      eventListen = Linking.addEventListener("url", (event) =>
+        handleUrl(event, manager)
+      );
+    }
+    return () => {
+      eventListen?.remove();
     };
+  }, [manager]);
 
-    Linking.addEventListener("url", handleUrl);
-  }, []);
+  // Handle OAuth callback
+  const handleUrl = (event: { url: string }, manager: OAuthManager) => {
+    const url = event.url;
+    if (url.startsWith("exp://192.168.2.83:8081")) {
+      const params = new URLSearchParams(url.split("?")[1]);
+      const oauth_verifier = params.get("oauth_verifier");
+
+      // Step 5: Exchange the request token for an access token
+      manager
+        .getAccessToken(oauth_verifier)
+        .then((success) => {
+          if (success) {
+            Alert.alert("Access Granted");
+            setHasAccessToken(true);
+          }
+        })
+        .catch((error) => {
+          console.error("Error getting access token", error);
+        });
+    }
+  };
 
   const initiateOAuthFlow = async () => {
     if (manager) {
@@ -76,6 +91,7 @@ const App = () => {
       ) : (
         <Button title="Login with OSCAR" onPress={initiateOAuthFlow} />
       )}
+      <Button title={"Call api"} onPress={callApi} />
     </View>
   );
 };
