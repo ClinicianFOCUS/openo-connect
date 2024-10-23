dba415d2ba548035122a7679bc5b47bfb6f86d96
Pemba Sherpa <norsangpemba@gmail.com>
2024-10-23 14:57:35 -0400

    custom modal for info

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,18 +1,45 @@
-import React from 'react';
-import { View, StyleSheet, ActivityIndicator } from 'react-native';
-import { useOAuth } from '@/hooks/useAuth';
+import React, { useLayoutEffect, useState } from 'react';
+import {
+  View,
+  StyleSheet,
+  ActivityIndicator,
+  TouchableOpacity,
+  Text,
+} from 'react-native';
 import { useAuthManagerStore } from '@/store/useAuthManagerStore';
 import AppointmentList from '@/components/AppointmentList';
 import FetchToken from '@/components/FetchToken';
 import Login from '@/components/LogIn';
 import useCurrentRoute from '@/hooks/useCurrentRoute';
+import { useNavigation } from 'expo-router';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import useModal from '@/hooks/useModal';
+import CustomModal from '@/components/CustomModal';
 
 const App = () => {
   // Get authentication state from AuthManager store
   const { hasAccessToken, hasUserCredentials, loading } = useAuthManagerStore();
+  const { modalVisible, setModalVisible } = useModal();
+  const navigation = useNavigation();
 
   // this sets the current route so that the app can return to it after authentication(biometrics)
   useCurrentRoute();
+
+  useLayoutEffect(() => {
+    navigation.getParent()?.setOptions({
+      headerRight: () => (
+        <View>
+          <TouchableOpacity onPress={showModal}>
+            <Ionicons name="information-circle-outline" size={36} />
+          </TouchableOpacity>
+        </View>
+      ),
+    });
+  }, []);
+
+  const showModal = () => {
+    setModalVisible(true);
+  };
 
   // Show loading indicator while checking authentication state and oauth manager has been initialized
   if (loading) {
@@ -30,6 +57,25 @@ const App = () => {
     return (
       <View style={styles.container}>
         <Login />
+        <CustomModal
+          title="Login Information"
+          modalVisible={modalVisible}
+          setModalVisible={setModalVisible}
+        >
+          <Text style={styles.title}>
+            Important: Make sure to provide O19 base url in setting before
+            logging in.
+          </Text>
+          <Text style={styles.paragraph}>
+            Please enter your login credentials to access the application. If
+            you don't have an account, please contact support for assistance.
+          </Text>
+          <Text style={styles.paragraph}>
+            Your credentials are stored securely on your device and are not
+            shared with anyone. Stored credentials are used for authentication
+            purpose in the future.
+          </Text>
+        </CustomModal>
       </View>
     );
   }
@@ -61,6 +107,21 @@ const styles = StyleSheet.create({
     left: '50%',
     transform: [{ translateX: -25 }, { translateY: -25 }],
     zIndex: 1,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  title: {
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: '#333',
+  },
+  paragraph: {
+    marginBottom: 10,
+    color: '#666',
+    lineHeight: 22,
   },
 });
 
